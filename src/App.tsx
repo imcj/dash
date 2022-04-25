0ef61b3577dc45fad914367965d2bbe7fe486b02
CJ <cj@iuantu.com>
2022-04-25 20:21:01 +0800

    init grid.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import GridLayout from "react-grid-layout";
+
 import './App.css';
+import "react-grid-layout/css/styles.css";
+import 'react-resizable/css/styles.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [width, setWidth] = useState<number>(0);
+
+  const layout = [
+    { i: "1", x: 0, y: 0, w: 5, h: 1}
+  ];
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GridLayout
+        className="layout"
+        layout={layout}
+        cols={12}
+        rowHeight={100}
+        width={width}
+      >
+        {layout.map(item => (
+          <div
+            key={item.i}
+            style={{
+              backgroundColor: "#efefef"
+            }}
+          >{item.i}</div>
+        ))}
+      </GridLayout>
     </div>
   );
 }
